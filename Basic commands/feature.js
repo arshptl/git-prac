b580d078150d6b52fec5e8d6f1bd524955bd4bc8
arshptl <arshpatel714@gmail.com>
2022-03-14 15:46:06 +0530

    updated feature.js

--- Basic commands/feature.js
+++ Basic commands/feature.js
@@ -2,5 +2,5 @@ console.log("feature 1");
 console.log("Rebase example");
 console.log("Rebase again");
 console.log("ola");
-
+console.log("performing interative rebasing");
 console.log("testing");
